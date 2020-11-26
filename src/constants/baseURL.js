284cdaab3549ae073bea75e8fad090f55fe7109e
Jilliane Vina <jvina@my.bcit.ca>
2020-11-26 11:24:37 -0800

    changed base url to hosted azure cms site

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,3 +1,3 @@
 // export const CMS_BASE_URL = 'https://dpyvweb.azurewebsites.net';
-export const CMS_BASE_URL = 'https://localhost:5001';
-export const CMS_MAP_BASE_URL = 'https://dpyvweb.azurewebsites.net';
+export const CMS_BASE_URL = 'https://vote-cms.azurewebsites.net/';
+export const CMS_MAP_BASE_URL = 'https://vote-cms.azurewebsites.net/';
